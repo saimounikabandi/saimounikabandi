1b8501ae2fd86260e9a26ca9d778a709cbe5fcaf
saimounikabandi <saimounikabandi@gmail.com>
2025-06-03 12:05:54 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,24 @@
+class main{
+    static boolean checkPangram(String s){
+        for(char ch='a';ch<='z';ch++){
+            boolean found=false;
+            for(int i=0;i<s.length();i++){
+                if(ch==Character.toLowerCase(s.charAt(i))){
+                    found=true;
+                    break;
+                }
+            }
+            if (found==false)
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[]args){
+        String s="The quick brown for jumps over the lazy dog";
+        if(checkPangram(s)==true)
+        System.out.println("true");
+        else
+        System.out.println("false");
+    }
+}
+
