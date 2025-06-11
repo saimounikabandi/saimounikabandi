b963eaa888735c8f576dc62d509d306547acf1b8
saimounika06 <saimounikabandi@gmail.com>
2025-06-11 14:20:24 +0530

    Add files via upload

---
+++ Selection sort.java
@@ -0,0 +1,39 @@
+ class Main {
+    static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            // Swap arr[i] and arr[min_idx]
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
+  
