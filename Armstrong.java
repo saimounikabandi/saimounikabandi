f166a67665def33757b8c6a0930724a2c8de9378
saimounika06 <saimounikabandi@gmail.com>
2025-06-16 14:01:19 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Main
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n value");
+        int n = sc.nextInt();
+        int temp=n;
+        int temp1=n;
+        int count=0;
+        int rem, res, sum=0;
+        while(n>0){
+            rem=n%10;
+            count+=1;
+            n=n/10;
+        }
+        while(temp>0)
+        {
+            rem=temp%10;
+            res=(int)Math.pow(rem,count);
+            sum= sum+ res;
+            temp=temp/10;
+        }
+        if(sum==temp){
+            System.out.println("yes it is Armstrong Number"+temp1);
+        }
+        else{
+            System.out.println("Not Armstrong");
+        }
+    }
+}
+    
+        
